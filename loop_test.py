5f11e3bcfa0c8199924f052b16e9cd2063efcda5
alamgir <alamgir88011@gmail.com>
2026-08-29 10:24:50 +0600

    Add python practice files

---
+++ loop_test.py
@@ -0,0 +1,15 @@
+
+
+for i in range(5):
+
+    env = input("Enter the environment")
+
+
+    if env == "prd":
+        print("it's online now")
+        
+    elif env == "test":
+        print("it's in testing stage now")
+        
+    else:
+        print("it's in devloping are")
